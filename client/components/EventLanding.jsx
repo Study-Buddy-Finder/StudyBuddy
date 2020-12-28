eb3b2d0877023ce71f9cd4a4cefb70f763806d5c
Vince Ho <vinceho@Vinces-MBP.lan>
2020-12-28 11:37:30 -0800

    done

--- client/components/EventLanding.jsx
+++ client/components/EventLanding.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import axios from 'axios';
 import EventList from "./EventList.jsx";
 import { AppContext } from "./ContextProvider";
+import Classinfo from "./Classinfo";
 //use axios to get the list
 //use hooks and use effect to set state 
 
@@ -18,40 +19,52 @@ function EventLanding() {
     //useEffect to update state and fetch data 
     //render event list 
     useEffect(()=> {
-        axios.get('http://localhost:3000/api/events')
+        axios.get(`http://localhost:3000/api/events/class/${currentClass_id}`)
             .then(res => {
                 setEventInfo(res.data);
-                // console.log(eventInfo);
+                console.log(eventInfo);
             })
             .catch (err => {
                 console.log(err);
             })
-    },[eventInfo.join(",")]);
+    },[currentClass_id]);
 
      useEffect(()=> {
-        axios.get('http://localhost:3000/api/classes')
+        axios.get(`http://localhost:3000/api/classes/${currentClass_id}`)
             .then(res => {
                 setClassInfo(res.data);
+                // console.log(res.data);
+                console.log(classinfo);
             })
             .catch (err => {
                 console.log(err);
             })
-    },[classinfo.join(",")]);
+    },[currentClass_id]);
     const eventList = [];
     for (let i = 0; i < eventInfo.length; i+= 1) {
         eventList.push(<EventList className ="eventList" key= {eventInfo[i]["event_id"]} name={eventInfo[i]["event_name"]} location={eventInfo[i]["event_location"]} ></EventList>)
     }
-   
+    const classInfo = [];
+    for (let i = 0; i < classinfo.length; i+= 1) {
+        classInfo.push(<Classinfo className ="classinfo" key= {classinfo[i]["class_id"]} subject={classinfo[i]["subject"]} name = {classinfo[i]["class_name"]} ></Classinfo>)
+    }
+    // <Classinfo className ="classinfo" key= {classinfo["class_id"]} subject={classinfo["subject"]} name = {classinfo["class_name"]} ></Classinfo>
     return (
-        <div>
-            Current Class id : {currentClass_id}
-            Event Landing
-            <div>
-                Class Info: 
+        <div className = "eventLandingContainer">
+            <div className ="classContainer">
+                Current Class id : {currentClass_id}
+                <div>
+                    {classInfo}
+                </div>
+
             </div>
-            <div>
-                {eventList}
+            <div className ="eventContainer">
+                Event Landing
+                <div className="events">
+                    {eventList}
+                </div>
             </div>
+            
         </div>
     )
 }
